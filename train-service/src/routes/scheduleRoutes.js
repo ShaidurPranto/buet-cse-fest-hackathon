ab2379382c4b35941ba4b4d3faed5e982059979a
ShaidurPranto <mdshaidurrahmanpranto@gmail.com>
2026-01-29 09:57:49 +0600

    databases structured

--- train-service/src/routes/scheduleRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { searchSchedules } from '../controllers/scheduleController.js';
-
-const router = express.Router();
-
-router.get('/schedules', searchSchedules);
-
-export default router;
